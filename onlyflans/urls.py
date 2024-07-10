e72d1aba8aecfd89b4b9c1d759952332615c83db
Magaly Apablaza <magaisa@gmail.com>
2024-07-09 22:08:31 -0400

    agrego app web con carpeta template y static

--- onlyflans/urls.py
+++ onlyflans/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web.views import base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', base, name="base"),
+    
+
 ]
